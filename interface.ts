935583b0448a111a32c11d0de7aeedb9e898b8e6
mclarabar <mcbp2@aluno.ifal.edu.br>
2025-09-10 17:54:33 -0300

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -8,6 +8,7 @@ interface ILivro{
     estoque: number;
 
     exibirDados(): void;
+    atualizarEstoque(estoque: number): void;
     //colocar os métodos aqui
 }
 
@@ -47,6 +48,15 @@ class LivroFisico implements ILivro{
         this.estoque += estoque;
         alert(`Ajustando o estoque, quantidade atual: ${this.estoque}.`);
     }
+
+    vender(quant: number): void {
+        if (this.estoque >= quant) {
+            this.estoque -= quant;
+            console.log(`Venda realizada com sucesso! Estoque atualizado, quantidade atual: ${this.estoque}.`);
+        } else {
+            console.log('Estoque insuficiente para a venda, tente outro livro!');
+        }
+    }
 }
 
 
@@ -85,6 +95,11 @@ class Ebook implements ILivro{
         console.log(`Estoque: ${this.estoque}`);
         console.log(`Tamanho: ${this.tamanho}`);
     }
+
+    atualizarEstoque(estoque: number): void {
+        this.estoque += estoque;
+        alert(`Ajustando o estoque, quantidade atual: ${this.estoque}.`);
+    }
 }
 
 
@@ -92,7 +107,27 @@ class Ebook implements ILivro{
 class GerenciaLivro{
     private livro: ILivro[] = []
 
-    //add livros, vender
+    adicionarLivro(livro: ILivro): void{
+        
+    }
+
+    excluirLivro(titulo: string): void{
+        
+    }
+
+    venderLivro(ltitulo: string): void{
+        
+    }
 
 }
+
+const gerenciadorLivro = new GerenciaLivro();
+
+const livro1 = new LivroFisico('Jogos Vorazes', 2008, '123456789', 50.84, 'Suzanne Collins', 'Rocco Jovens Leitores', 5);
+const livro2 = new LivroFisico('Jogos Vorazes: Em Chamas', 2009, '121212121', 51.94, 'Suzanne Collins', 'Rocco Jovens Leitores', 5);
+const livro3 = new LivroFisico('Jogos Vorazes: A Esperança', 2010, '987654321', 50.03, 'Suzanne Collins', 'Rocco Jovens Leitores', 5);
+
+const livro4 = new Ebook('Jogos Vorazes', 2008, '123456789', 31.26, 'Suzanne Collins', 'Rocco Jovens Leitores', 5, 2.8);
+const livro5 = new Ebook('Jogos Vorazes: Em Chamas', 2009, '121212121', 31.26, 'Suzanne Collins', 'Rocco Jovens Leitores', 5, 2.9);
+const livro6 = new Ebook('Jogos Vorazes: A Esperança', 2010, '987654321', 31.26, 'Suzanne Collins', 'Rocco Jovens Leitores', 6, 2.6);
 
